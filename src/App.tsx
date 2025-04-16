778a96c6d4e33f645d30a380bec1e92a537999f0
Etan Cohn <etan.cohn@gmail.com>
2025-04-15 22:32:34 -0400

    added svg title

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { SoundCloudPlaylist } from './SoundCloudPlayer';
 import MemberAvatars from './MemberAvatars';
 import ParticlesBackground from './ParticlesBackground';
+import BandTitle from './BandTitle';
 
 function App() {
 
     return (
       <div className='app-container'>
         <ParticlesBackground />
+        <BandTitle />
         <MemberAvatars />
         <SoundCloudPlaylist
           playlistUrl='https://soundcloud.com/etan-cohn-996334839/sets/boston-band'
